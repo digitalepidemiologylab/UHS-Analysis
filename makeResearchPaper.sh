b319b424bcd54f12e86c3c6cadc286c41153c7a9
ToddBodnar <tjb5215@psu.edu>
2013-11-08 00:48:15 -0500

    Automated paper building

---
+++ makeResearchPaper.sh
@@ -0,0 +1,11 @@
+cd code
+bash runMe.sh
+cd ..
+
+latex --output-format=pdf Bodnar_www2014
+latex --output-format=pdf Bodnar_www2014
+
+BibTex Bodnar_www2014
+
+latex --output-format=pdf Bodnar_www2014
+latex --output-format=pdf Bodnar_www2014
