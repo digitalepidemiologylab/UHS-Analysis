990d3706b52576c7551954c6dcbf0058e69440e7
ToddBodnar <tjb5215@psu.edu>
2013-11-09 00:59:00 -0500

    Outlined signal methods

--- code/frequencyGraph.r
+++ code/frequencyGraph.r
@@ -10,17 +10,64 @@ postscript("allFrequencies.eps")
 
 plot(NA,xlim=c(-6,6),ylim=c(0,1))
 
+#find average frequency
 total = (1:50)*0
 found = (1:50)*0
+
+
+#for anova
+type = c()
+instances = c()
+
+#for paired
+beforePair = c()
+atPair = c()
+afterPair = c()
 
 for ( id in 1:length(users$id))
 {
 res = fetch(dbSendQuery(mydb, paste('select count(*) as ct, month_index -',users$month_index[id],'as month_index from tweets where user=',users$userID[id],' group by month_index')))
-
+res$ct_old = res$ct
 res$ct = (res$ct - min(res$ct))/(max(res$ct) - min(res$ct))
 total[res$month_index+25] = total[res$month_index+25] + res$ct
 lines(res$month_index, res$ct,col=rgb(0,0,0))
 found[res$month_index+25] = found[res$month_index+25]+1
+
+instances = c(instances, res$ct[which(res$month_index > -4  & res$month_index < 0)])
+
+if(length(which(res$month_index > -4  & res$month_index < 0)) > 0){
+for(tmp in 1:length(which(res$month_index > -4  & res$month_index < 0)))
+{
+	type=c(type,"before")
+}}
+
+instances = c(instances, res$ct[which(res$month_index > -1  & res$month_index < 1)])
+
+if(length(which(res$month_index > -1  & res$month_index < 1)) > 0){
+for(tmp in 1:length(which(res$month_index > -1  & res$month_index < 1)))
+{
+        type=c(type,"at")
+}}
+
+instances = c(instances, res$ct[which(res$month_index > 0  & res$month_index < 4)])
+
+if(length(which(res$month_index > 0  & res$month_index < 4)) > 0){
+for(tmp in 1:length(which(res$month_index > 0  & res$month_index < 4)))
+{
+        type=c(type,"after")
+}}
+
+if(length(which(res$month_index > 0  & res$month_index < 4)) > 0 & length(which(res$month_index > -1  & res$month_index < 1)) > 0 & length(which(res$month_index > -4  & res$month_index < 0)) > 0)
+{
+beforePair = c(beforePair,mean(res$ct_old[which(res$month_index > -4  & res$month_index < 0)]))
+
+atPair = c(atPair, res$ct_old[which(res$month_index == 0)])
+
+afterPair = c(afterPair ,mean(res$ct_old[which(res$month_index > 0  & res$month_index < 4)]))
+
+
+}
+
 }
 
 dev.off()
