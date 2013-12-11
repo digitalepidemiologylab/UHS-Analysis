bdec69e3b8f5b1b14aa4badda4da9f2f653d60e4
ToddBodnar <tjb5215@psu.edu>
2013-12-11 13:30:51 -0500

    ROC for keywords dm now looks a bit prettier

--- code/roc_key_dm.r
+++ code/roc_key_dm.r
@@ -1,6 +1,6 @@
 setEPS();
 postscript("key_dm_roc.eps")
-
+par(cex.lab=2)
 Sys.setenv(NOAWT=1)
 
 library("RWeka")
@@ -10,7 +10,7 @@ files = c("../data/private/roc_keyword_dm_10_j48.arff","../data/private/roc_keyw
 colors = c("red","blue","green","orange","purple")
 colors = c(colors,colors,colors)
 
-linetype = c(1,1,1,1,1,2,2,2,2,2,4,4,4,4,4)
+linetype = c(1,1,1,1,1,2,2,2,2,2,3,3,3,3,3)
 
 plot(c(0,1),c(0,1),lty=2, xlim=c(0,1),ylim=c(0,1),type="l", xlab = "False Positive", ylab = "True Positive")
 
@@ -19,7 +19,7 @@ for(x in 1:length(files))
  read.arff(files[x]) -> r
 make.names(names(r)) -> names(r)
 
-lines(r$False.Positives/max(r$False.Positives),r$True.Positives/max(r$True.Positives), type="l", col=colors[x], lwd=2, lty = linetype[x])
+lines(r$False.Positives/max(r$False.Positives),r$True.Positives/max(r$True.Positives), type="l", col=colors[x], lwd=4, lty = linetype[x])
 }
 
 dev.off()
