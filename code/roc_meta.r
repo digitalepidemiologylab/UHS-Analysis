1c8541b2d21e7f3da7d7394d325fc3354d824485
ToddBodnar <tjb5215@psu.edu>
2013-12-11 15:06:30 -0500

    misc edits, initial writing for meta classifier

--- code/roc_meta.r
+++ code/roc_meta.r
@@ -5,6 +5,20 @@ source("oneDimensionAnomalyDetection.r")
 roc(sick_times,not_sick_times, col="red",lwd=2.5, xlab="False Positive Rate", ylab = "True Positive Rate")
 lines(c(0,0,1),c(0,17/35,1),col="blue",lwd=2.5)
 
-legend(.6,.4, c("Tweet Classifier", "Keyword Classifier", "Expert Keyword", "Mined Keyword","Network Classifier", "Baseline"), lty=c(1,1,1,1,1,2), col = c("Blue","Red","Orange","Green","Purple","Black" ), lwd=2.5)
+
+library("RWeka")
+
+ read.arff("../data/private/roc_keyword_dm_100_naivebayes.arff") -> r
+make.names(names(r)) -> names(r)
+
+lines(r$False.Positives/max(r$False.Positives),r$True.Positives/max(r$True.Positives), type="l", col="green", lwd=2.5, lty = 1)
+
+
+ read.arff("../data/roc_keyword_expert_naivebayes.arff") -> r
+make.names(names(r)) -> names(r)
+
+lines(r$False.Positives/max(r$False.Positives),r$True.Positives/max(r$True.Positives), type="l", col="orange", lwd=2.5, lty = 1)
+
+legend(.6,.4, c("Human Classifier", "Anomaly Classifier", "Expert Keyword", "Mined Keyword","Network Classifier", "Baseline"), lty=c(1,1,1,1,1,2), col = c("Blue","Red","Orange","Green","Purple","Black" ), lwd=2.5)
 
 dev.off()
