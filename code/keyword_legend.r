e8a02451d51a1b48bdb0fe97af0e1f992fc2d6b3
ToddBodnar <tjb5215@psu.edu>
2013-12-19 17:31:07 -0500

    keyword legend looks nicer

--- code/keyword_legend.r
+++ code/keyword_legend.r
@@ -1,6 +1,6 @@
 setEPS()
 
-postscript("keyword_legend.eps",width=1064/150,height=52/150)
+postscript("keyword_legend.eps",width=1264/150,height=52/150)
 
 
 
@@ -14,7 +14,7 @@ par(mar=c(0.0,0,0,0))
 
 plot(1, type="n", axes=F, xlab="", ylab="",xlim=c(0,12),ylim=c(-0.25,2))
 
-text(2*(1:5),y=0,labels=labels,adj=c(.5,0))
+text(2*(1:5),y=-.1,labels=labels,adj=c(.5,0))
 
 rect(2*(1:5)-.9,1,2*(1:5)+.9,2,col=colors)
 
--- code/runMe.sh
+++ code/runMe.sh
@@ -4,6 +4,9 @@ R CMD BATCH roc_meta.r &
 R CMD BATCH roc_key_exp.r &
 R CMD BATCH roc_key_dm.r &
 
+R CMD BATCH keyword_legend.r &
+
+
 wait
 
 mv *.eps ../figs/
